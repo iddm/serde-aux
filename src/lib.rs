3e6e942b8b31bddc3e0110df8798c81850249dd1
ta32 <ir.ta.work@gmail.com>
2021-09-06 22:39:35 +1000

    serde aux function to get names for fields as they are serialized
    from https://github.com/serde-rs/serde/issues/1110

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,18 @@
 #![deny(missing_docs)]
 #![deny(warnings)]
 
+#[macro_use] extern crate serde;
+
 /// Contains helpers for the containers.
 pub mod container_attributes;
 /// Contains helpers for the fields.
 pub mod field_attributes;
+/// Contains helpers to get serialization names for struct fields and enum variants as they are serialized.
+pub mod serde_introspection;
 
 /// Prelude module, contains the most needed helpers from this library.
 pub mod prelude {
     pub use crate::container_attributes::*;
     pub use crate::field_attributes::*;
+    pub use crate::serde_introspection::*;
 }
